40ec7590fd0ee4fa8ed4e00519e7e5f317338ba7
wise-king-sullyman <asullivan96@live.com>
2020-09-28 23:48:40 -0400

    Added Gallows class for managing the drawing of the gallows and keeping count of gallows stage

--- hangman.rb
+++ hangman.rb
@@ -3,16 +3,22 @@
 # Responsible for drawing the different stages of the hangmans gallows
 class Gallows
   def initialize
-    @hangman_stages = File.readlines "hangman_stages.txt"
+    @hangman_stages = File.readlines 'hangman_stages.txt'
     @stage = 0
   end
 
   def draw
-    @stage *= 7
-    puts @hangman_stages.slice(@stage..@stage + 6)
+    stage_times_offset = @stage * 7
+    puts @hangman_stages.slice(stage_times_offset..stage_times_offset + 6)
   end
 
-  def next
+  def next_stage
     @stage += 1
   end
+
+  def dropsies?
+    true if @stage == 6
+  end
 end
+
+
